b05c42f3d7945dd2d9dbfef5c3ef6e940696ef5a
Patricia Byrro <pbbyrro@gmail.com>
2024-03-15 16:19:51 -0300

    feat - repository and CRUD
    Co-authored-by: Sabrina S Furtado <sabrina.s.furtado@gmail.com>
    Co-authored-by: Douglas Queiroz <douglas.lima.queiroz@gmail.com>
    Co-authored-by: CajueiroCaio <caiovcajueiro@gmail.com>
    Co-authored-by: Gustavo Kamimura <gustavokamimura@hotmail.com>

--- src/main/java/com/generation/descontoexpresso/model/Categoria.java
+++ src/main/java/com/generation/descontoexpresso/model/Categoria.java
@@ -1,6 +1,7 @@
 package com.generation.descontoexpresso.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,35 +13,37 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "O campo nome não pode ser vazio!")
+    @NotBlank(message = "O campo nome não pode ser vazio!")
     @Size(min = 5, max = 100, message = "Nome precisa de no minímo 5 caracteres e no máximo 100")
     private String nome;
 
-    @NotNull
+    @NotBlank
     @Size(min = 10, max = 255, message = "Descrição precisa de no minímo 10 caracteres e no máximo 255")
     private String descricao;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getDescricao() {
-        return descricao;
-    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+    
 }
